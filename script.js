b4a84838416a11736eb252b4671760567764a143
Miguel Escobar <mae2136@gmail.com>
2022-02-27 13:59:05 -0600

    - Went back to original source bootstrap & jQuery. - Created script.js file. - Add current day to top of planner.

---
+++ script.js
@@ -0,0 +1,8 @@
+// Display current day at top of planner (in #currentDay)
+var today = moment();
+$(`#currentDay`).text(today.format("dddd, MMMM Do"));
+// Create table with a standard timeblocks (9AM - 5PM) of current day in container.
+// Color code each timeblock to indicate past (grey), present (red), or future (green)
+// When click into timeblock (event.target), user can type in an event.
+// When click in to save button for the timeblock, then text for event is saved in local storage.
+// When refresh page, local storage populates daily plan with saved events
