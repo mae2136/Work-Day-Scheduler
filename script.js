d688f2665e4953e889c50aa5e891e5c7356ffe5d
Miguel Escobar <mae2136@gmail.com>
2022-02-27 23:02:23 -0600

    index.html: - Add table elements for time blocks from bootstrap. - Start time block creation function.

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 var today = moment();
 $(`#currentDay`).text(today.format("dddd, MMMM Do"));
 // Create table with a standard timeblocks (9AM - 5PM) of current day in container.
+function createBlocks(today) {
+    // Run function 9 AM - 5 PM (9 iterations)
+    console.log(today);
+}
+
+// $(`#planner`).
 // Color code each timeblock to indicate past (grey), present (red), or future (green)
 // When click into timeblock (event.target), user can type in an event.
 // When click in to save button for the timeblock, then text for event is saved in local storage.
