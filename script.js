8cdd75fa3a6107f23f0eb3ca37b7136cc44005c7
Miguel Escobar <mae2136@gmail.com>
2022-02-28 22:28:53 -0600

    index.html: - Added missing </div> to fix 9AM button format. - changed userText to timeBlock class.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var today = moment();
 $(`#currentDay`).text(today.format("dddd, MMMM Do"));
 
 // Color code each timeblock to indicate past (grey), present (red), or future (green)
+
+
 // When click into timeblock (event.target), user can type in an event.
 // When click in to save button for the timeblock, then text for event is saved in local storage.
 // When refresh page, local storage populates daily plan with saved events
