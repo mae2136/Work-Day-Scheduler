7ba1491b852b80c86f5283b98fab98b6555c7e48
Miguel Escobar <mae2136@gmail.com>
2022-02-28 21:29:58 -0600

    index.html: - Change form to textarea. - Format inputs, add save button, add time component.
    script.js:
    - Initial thoughts on appending time so as not to create a massive html doc.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Display current day at top of planner (in #currentDay)
 var today = moment();
+var dailyTime = [`10 AM`, `11 AM`, `12 PM`, `1 PM`, `2 PM`, `3 PM`, `4 PM`, `5 PM`]
 $(`#currentDay`).text(today.format("dddd, MMMM Do"));
 // Create table with a standard timeblocks (9AM - 5PM) of current day in container.
-function createBlocks(today) {
+function createBlocks(dailyTime) {
     // Run function 9 AM - 5 PM (9 iterations)
-    console.log(today);
+    console.log(dailyTime);
+    var newRow = $(`#planner`);
+    console.log(newRow)
+    // Change newRow's #time to value of dailyTime[i]
 }
 
-// $(`#planner`).
+createBlocks(dailyTime);
 // Color code each timeblock to indicate past (grey), present (red), or future (green)
 // When click into timeblock (event.target), user can type in an event.
 // When click in to save button for the timeblock, then text for event is saved in local storage.
