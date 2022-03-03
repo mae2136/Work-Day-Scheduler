dc7fe9341d529e7c8f835d2bd5f0684cabebee35
Miguel Escobar <mae2136@gmail.com>
2022-03-02 22:17:51 -0600

    script.js: - Added local storage functionality (get and set).

--- script.js
+++ script.js
@@ -2,11 +2,24 @@
 var today = moment();
 var eventList = []
 
+var eventStorage = {
+    events: eventList,
+    date: today,
+}
+
+var test = JSON.parse(localStorage.getItem(`eventStorage`)); 
+
+if ((test != null) && (today.isSame(test.date, `day`))) {
+    console.log(`There is data stored and it belongs to today`)
+    eventStorage.events = test.events
+    console.log(eventStorage);
+}
+
 $(`#currentDay`).text(today.format("dddd, MMMM Do"));
 
 // Color code each timeblock to indicate past (grey), present (red), or future (green)
-console.log(today.format(`hh A`))
-console.log($(`.container`).children().eq(5).children().eq(0).text())
+// console.log(today.format(`hh A`))
+// console.log($(`.container`).chi+ldren().eq(5).children().eq(0).text())
 
 // When click into timeblock (event.target), user can type in an event.
 // Done by default with <textbox>
@@ -20,7 +33,12 @@ function saveEvent(event) {
     var event = currentDiv.children().eq(1).val();
     var eventLocation = currentDiv.children().eq(0).attr(`id`);
     eventList[eventLocation] = event;
-    console.log(eventList);
+    // console.log(eventList);
+    eventStorage.events = eventList;
+    eventStorage.date = today;
+    console.log(eventStorage);
+    localStorage.setItem(`eventStorage`, JSON.stringify(eventStorage));
     return
 }
+
 // When refresh page, local storage populates daily plan with saved events
